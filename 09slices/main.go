63f6bd49905b852c0286b785140fad817ca166bb
Arpan Das <dasarpan471@gmail.com>
2023-07-03 07:52:38 +0530

    delete element in a slice

--- 09slices/main.go
+++ 09slices/main.go
@@ -37,4 +37,13 @@ func main() {
 
 	fmt.Println(highScores)
 
+	var courses = []string{"reactjs", "javascript", "swift", "ruby"}
+
+	fmt.Println(courses)
+
+	var index int = 2
+
+	courses = append(courses[:index], courses[index+1:]...)
+
+	fmt.Println(courses)
 }
