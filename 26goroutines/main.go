f51c66757e9764a3a8177a47f9b464c6d84db599
Arpan Das <dasarpan471@gmail.com>
2023-07-09 08:29:58 +0530

    setting up for parallelism

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func runner1() {
+	fmt.Println("It is the first runner")
+}
+func runner2() {
+	fmt.Println("It is the second runner")
+}
+
+func execute() {
+	runner1()
+	runner2()
+}
+
+func main() {
+	execute()
 }
