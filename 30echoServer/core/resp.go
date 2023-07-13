59dff5deccc70a1421c8d461e910a2b2329778d9
Arpan Das <dasarpan471@gmail.com>
2023-07-13 07:32:46 +0530

    resp implementation

--- 30echoServer/core/resp.go
+++ 30echoServer/core/resp.go
@@ -2,17 +2,69 @@ package core
 
 import "errors"
 
+func readLength(data []byte) (int, int) {
+	pos, length := 0, 0
+	for pos = range data {
+		b := data[pos]
+		if !(b >= '0' && b <= '9') {
+			return length, pos + 2
+		}
+		length = length*10 + int(b-'0')
+	}
+	return 0, 0
+}
+
 func readSimpleString(data []byte) (string, int, error) {
 	pos := 1
 
 	for ; data[pos] != '\r'; pos++ {
 	}
-
 	return string(data[1:pos]), pos + 2, nil
 }
 
 func readError(data []byte) (string, int, error) {
 	return readSimpleString(data)
+}
+
+func readInt64(data []byte) (int64, int, error) {
+	pos := 1
+
+	var value int64 = 0
+
+	for ; data[pos] != '\r'; pos++ {
+		value = value*10 + int64(data[pos]-'0')
+	}
+	return value, pos + 2, nil
+
+}
+
+func readBulkString(data []byte) (string, int, error) {
+	pos := 1
+
+	len, delta := readLength(data)
+
+	pos += delta
+
+	return string(data[pos:(pos + len)]), pos + len + 2, nil
+}
+
+func readArray(data []byte) (interface{}, int, error) {
+	pos := 1
+
+	count, delta := readLength(data[pos:])
+
+	pos += delta
+
+	var elems []interface{} = make([]interface{}, count)
+	for i := range elems {
+		elem, delta, err := DecodeOne(data[pos:])
+		if err != nil {
+			return nil, 0, err
+		}
+		elems[i] = elem
+		pos += delta
+	}
+	return elems, pos, nil
 }
 
 func DecodeOne(data []byte) (interface{}, int, error) {
@@ -24,6 +76,20 @@ func DecodeOne(data []byte) (interface{}, int, error) {
 		return readSimpleString(data)
 	case '-':
 		return readError(data)
+	case ':':
+		return readInt64(data)
+	case '$':
+		return readBulkString(data)
+	case '*':
+		return readArray(data)
 	}
 	return nil, 0, nil
+}
+
+func Decode(data []byte) (interface{}, error) {
+	if len(data) == 0 {
+		return nil, errors.New("no data")
+	}
+	value, _, err := DecodeOne(data)
+	return value, err
 }
