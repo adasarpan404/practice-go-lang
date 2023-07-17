4e79d264360a39433022e6dd266c734b79014745
Arpan Das <dasarpan471@gmail.com>
2023-07-17 08:39:20 +0530

    factory method completed

---
+++ 33DesignPatterns/factory/readme.md
@@ -0,0 +1,6 @@
+# Factory Method
+
+    - It is a creational Design Patterns
+    - It provides an interface for creating objects in superclass.
+    - It allows subclasses to alter the type of object that will created.
+
--- 33DesignPatterns/main.go
+++ 33DesignPatterns/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "github.com/adasarpan404/designPatterns/factory"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/adasarpan404/designPatterns/factory"
+)
 
 func main() {
+	fmt.Println("Design Patterns in GO")
 
-	factory.FactoryUsage()
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	input = strings.TrimSpace(input) // Remove leading/trailing whitespaces
+
+	if strings.EqualFold(input, "factory") {
+		fmt.Println(true)
+		factory.FactoryUsage()
+	} else {
+		fmt.Println("Invalid input.")
+	}
 }
