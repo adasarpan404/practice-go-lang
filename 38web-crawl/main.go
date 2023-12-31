9c0efd6ab6a098e94bf73591ae51b1e893bf875e
Arpan Das <dasarpan471@gmail.com>
2023-12-31 09:44:34 +0530

    web crawl

---
+++ 38web-crawl/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func scrape() {
+	res, err := http.Get("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("a").Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
+}
+func main() {
+	scrape()
+}
